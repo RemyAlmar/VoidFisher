ff0027f3f54b3645b7f49af3cd9a99a9666bea04
RemyAlmar <r.almar1997@gmail.com>
2024-10-10 13:22:10 +0200

    [ADD] Solution Creation

---
+++ Source/VoidFisher/VoidFisher.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VoidFisher.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VoidFisher, "VoidFisher" );
